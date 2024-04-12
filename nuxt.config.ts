8ba46c16a8ea4575071a056b3b63f909536afe20
Tong Siyuan <tongsiyuan@bolegames.com>
2024-04-12 18:07:55 +0800

    feat: init content nuxtg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/fonts',
-    '@vueuse/nuxt'
-  ],
+  extends: [process.env.NUXT_UI_PRO_PATH || "@nuxt/ui-pro"],
+  modules: ["@nuxt/ui", "@nuxt/fonts", "@vueuse/nuxt", "@nuxt/content"],
   ui: {
-    icons: ['heroicons', 'simple-icons'],
-    safelistColors: ['primary', 'red', 'orange', 'green']
+    icons: ["heroicons", "simple-icons"],
+    safelistColors: ["primary", "red", "orange", "green"],
   },
   devtools: {
-    enabled: true
-  }
-})
+    enabled: true,
+  },
+});
