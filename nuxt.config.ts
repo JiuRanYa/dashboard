4c5f1c2c939256e4ca6619607198900ff27274c3
Tong Siyuan <tongsiyuan@bolegames.com>
2024-04-12 18:11:42 +0800

    feat: studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [process.env.NUXT_UI_PRO_PATH || "@nuxt/ui-pro"],
-  modules: ["@nuxt/ui", "@nuxt/fonts", "@vueuse/nuxt", "@nuxt/content"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@vueuse/nuxt",
+    "@nuxt/content",
+    "@nuxthq/studio",
+  ],
   ui: {
     icons: ["heroicons", "simple-icons"],
     safelistColors: ["primary", "red", "orange", "green"],
